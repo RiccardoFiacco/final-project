8fd868714e1badbfaf821b85e0b85192c28929e1
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-04-09 23:15:08 +0200

    changes

--- final-project/src/main/java/com/example/demo/FinalProjectApplication.java
+++ final-project/src/main/java/com/example/demo/FinalProjectApplication.java
@@ -1,13 +1,13 @@
-package finalproject.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 
 }
--- final-project/src/test/java/com/example/demo/FinalProjectApplicationTests.java
+++ final-project/src/test/java/com/example/demo/FinalProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package finalproject.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FinalProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
