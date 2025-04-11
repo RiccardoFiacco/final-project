4103babf0403edd28f1f7281da8b4f5d35e97a68
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-04-12 00:51:56 +0200

    add DB user detail service and start security config class

---
+++ final-project/src/main/java/com/example/demo/security/DataBaseUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.demo.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.model.User;
+
+public class DataBaseUserDetails implements UserDetails {
+
+    public final Integer id;
+    public final String username;
+    public final String password;
+    public final Set<GrantedAuthority> authorities;
+
+    public DataBaseUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.authorities = new HashSet<GrantedAuthority>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
